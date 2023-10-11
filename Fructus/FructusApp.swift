6060c57fe0ac7ec9584d654ca3c4370c4ffcf0c5
Pratiksha <pratiksha.mohadare9@gmail.com>
2023-10-11 13:36:12 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Pratiksha on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
